0a795dc2b4aabc2dacb8690f626ca2b1aec0b02c
Kairis <80596049+Johnson-Tung@users.noreply.github.com>
2022-09-13 00:03:34 -0700

    feat: YouTube data collection
    Core code to collect channel and video data using the YouTube Data API

---
+++ holoanalytics/datacollection/youtube/videos.py
@@ -0,0 +1,320 @@
+"""Collection of YouTube video data using the YouTube Data API
+
+This module provides tools to request YouTube video data for specified channels using the YouTube Data API,
+extract relevant data from the API's responses, and return the results. By default, copies of the data are exported
+to csv files.
+
+Relevant data includes:
+    - Video ids
+    - Video attributes
+    - Video stats
+
+Functions:
+    - get_video_data: Retrieves relevant video data of Hololive Production members' videos.
+    - get_video_ids: Extracts video ids from API responses.
+    - get_video_stats: Extracts video statistics from API responses.
+    - get_video_attributes: Extracts video attributes from API responses.
+"""
+
+import numpy as np
+import pandas as pd
+from holoanalytics import definitions as df
+from holoanalytics.datacollection.youtube import youtube_api
+from holoanalytics.utils import export
+
+
+def get_video_data(client, starting_data, playlist_ids_df, max_results=50, export_data=True):
+    """Retrieves relevant video data of Hololive Production members' videos using each members' playlist id.
+
+    Relevant data includes:
+    - Video ids
+    - Video attributes
+    - Video stats
+
+    Args:
+        client: YouTube Data API client.
+        starting_data: Starting data file imported as a Pandas DataFrame.
+        playlist_ids_df: Pandas Series containing uploads playlist ids for each channel for which video data is
+                         being collected for.
+        max_results: Integer representing the maximum number of results that the API can return in a single response.
+                     Default = 50, the highest possible value.
+        export_data: Boolean specifying whether collected data is to be exported. Default = True.
+
+    Returns:
+        all_video_ids: Dictionary of Pandas DataFrames containing video ids for each Hololive Production member data
+                       is being collected for.
+        all_video_attributes: Dictionary of Pandas DataFrames containing video attributes for each Hololive Production
+                              member.
+        all_video_stats: Dictionary of Pandas DataFrames containing video statistics for each Hololive Production
+                              member.
+    """
+
+    all_video_ids = {}
+    all_video_attributes = {}
+    all_video_stats = {}
+    member_dir_path = None
+
+    id_type = playlist_ids_df.columns[1]  # Get type of playlist id, i.e. uploads or members
+    names_and_ids = _link_names_to_ids(starting_data, playlist_ids_df, id_type)
+
+    for member_name, playlist_id in zip(names_and_ids['name'], names_and_ids[id_type]):
+
+        if export_data is True and df.SESSION_PATH is not None:
+            member_dir_path = export.create_directory(df.SESSION_PATH / 'Video', member_name.replace(' ', '_'),
+                                                      add_date=False)
+
+        playlist_responses = youtube_api.request_data(client, 'playlistItems', ids=playlist_id, max_results=max_results)
+        all_video_ids[member_name] = get_video_ids(member_name, playlist_responses, export_data)
+
+        if all_video_ids[member_name] is None:
+            all_video_ids = _video_ids_missing(all_video_ids, member_name, member_dir_path)
+        else:
+            all_video_attributes, all_video_stats = _video_ids_exist(client, all_video_ids, all_video_attributes,
+                                                                     all_video_stats, member_name, max_results,
+                                                                     export_data)
+
+    return all_video_ids, all_video_attributes, all_video_stats
+
+
+def _link_names_to_ids(starting_data, collected_data, id_type):
+    """Links the names of Hololive Production members to their respective ids, e.g. playlist ids.
+
+    Args:
+        starting_data: Starting data file imported as a Pandas DataFrame.
+        collected_data: Pandas DataFrame that the starting data will be merged with.
+        id_type: String specifying the type of id (i.e. column) that the DataFrames will be merged on.
+
+    Returns:
+        names_and_ids: Pandas DataFrame with 2 columns: member name and id_type.
+    """
+
+    names_and_ids = starting_data.merge(collected_data, left_on='youtube_channel_id',
+                                        right_on='channel_id')[['name', id_type]]
+
+    return names_and_ids
+
+
+def _video_ids_missing(all_video_ids, member_name, member_dir_path):
+    """Removes the specified Hololive Production member from the data collection process due to a lack of video ids.
+
+    PRIVATE function.
+
+    Args:
+        all_video_ids: Dictionary of Pandas DataFrames containing video ids, collected so far, of Hololive Production
+                       members' videos.
+        member_name: String specifying the name of the Hololive Production member whose videos data is being
+                     collected for.
+        member_dir_path: Path object specifying the absolute path to the current member's directory.
+
+    Returns:
+        all_video_ids: Dictionary of Pandas DataFrames updated with video ids for the current member.
+    """
+
+    del all_video_ids[member_name]
+    if member_dir_path is not None:
+        member_dir_path.rmdir()
+        print(f"'{member_dir_path.name}' has been successfully deleted from '{member_dir_path.parent}'.")
+
+    return all_video_ids
+
+
+def _video_ids_exist(client, all_video_ids, all_video_attributes, all_video_stats, member_name,
+                     max_results, export_data):
+    """Retrieves video attributes and statistics for videos of the current Hololive Production member.
+
+    PRIVATE function.
+
+    Args:
+        client:
+        all_video_ids: Dictionary of Pandas DataFrames containing video ids, collected so far, of Hololive Production
+                       members' videos.
+        all_video_attributes: Dictionary of Pandas DataFrames containing video attributes, collected so far, of
+                              Hololive Production members' videos.
+        all_video_stats: Dictionary of Pandas DataFrames containing video statistics, collected so far, of Hololive
+                         Production members' videos.
+        member_name: String specifying the name of the Hololive Production member whose videos data is being
+                     collected for.
+        max_results: Integer representing the maximum number of results that the API can return in a single response.
+                     Default = 50, the highest possible value.
+        export_data: Boolean specifying whether collected data is to be exported. Default = True.
+
+    Returns:
+        all_video_attributes: Dictionary of Pandas DataFrames updated with video attributes for the current member.
+        all_video_stats: Dictionary of Pandas DataFrames updated with video statistics for the current member.
+    """
+
+    video_ids = all_video_ids[member_name]['video_id']
+    video_responses = youtube_api.request_data(client, 'video', video_ids, max_results)
+    all_video_attributes[member_name] = get_video_attributes(member_name, video_responses, export_data)
+    all_video_stats[member_name] = get_video_stats(member_name, video_responses, export_data)
+
+    return all_video_attributes, all_video_stats
+
+
+def get_video_ids(member_name, responses, export_data=True):
+    """Extracts video ids for the specified Hololive Production member from the API's responses.
+
+        This function extracts YouTube video ids from YouTube Data API responses, exports a copy of the data
+        (unless instructed otherwise), and returns a Pandas DataFrame containing the collected data.
+
+        Args:
+            member_name: String specifying the name of the Hololive Production member whose videos data is being
+                         collected for.
+            responses: List of YouTube Data API responses to requests for playlistItem data.
+            export_data: Boolean specifying whether collected data is to be exported. Default = True.
+
+        Returns:
+            data: Pandas DataFrame containing video ids for the current member and date and time each video was added
+                  to the playlist.
+        """
+
+    video_ids = []
+    added_to_playlist = []
+
+    if not responses:
+        print(f"{member_name} has no videos available in this playlist.")
+        return
+
+    for response in responses:
+        results = response['items']
+        for result in results:
+            video_ids.append(result['snippet']['resourceId']['videoId'])
+            # Note: 'publishedAt' refers to the date and time that the video was added to the playlist,
+            # not necessarily when the video was published to YouTube.
+            added_to_playlist .append(result['snippet']['publishedAt'])
+
+    data = pd.DataFrame(zip(video_ids, added_to_playlist), columns=['video_id', 'added_to_playlist'])
+
+    _export_video_data(member_name, data, export_data, 'uploads_video_ids')
+
+    return data
+
+
+def get_video_stats(member_name, responses, export_data=True):
+    """Extracts video statistics for the specified Hololive Production member's videos from the API's responses.
+
+    This function extracts YouTube video statistics from YouTube Data API responses, exports a copy of the data
+    (unless instructed otherwise), and returns a Pandas DataFrame containing the collected data.
+
+    Args:
+        member_name: String specifying the name of the Hololive Production member whose videos data is being
+                     collected for.
+        responses: List of YouTube Data API responses to requests for video data.
+        export_data: Boolean specifying whether collected data is to be exported. Default = True.
+
+    Returns:
+        data: Pandas DataFrame containing video ids and corresponding video statistics for the current member.
+    """
+
+    video_ids = []
+    view_counts = []
+    like_counts = []
+    likes_enabled = []
+    comment_counts = []
+    comments_enabled = []
+
+    for response in responses:
+        results = response['items']
+        for result in results:
+            video_ids.append(result['id'])
+            # 'viewCount' property not available for members-only videos, resulting in KeyErrors
+            view_counts.append(result['statistics'].get('viewCount', np.NaN))
+            # 'likeCount' property not available if likes are disabled
+            like_counts.append(result['statistics'].get('likeCount', np.NaN))
+            # Provide additional context in event of likeCount of zero
+            likes_enabled.append(('likeCount' in result['statistics']))
+            # 'commentCount' property not available if comments are disabled
+            comment_counts.append(result['statistics'].get('commentCount', np.NaN))
+            # Provide additional context in event of commentCount of zero
+            comments_enabled.append(('commentCount' in result['statistics']))
+
+    data = pd.DataFrame(zip(video_ids, view_counts, like_counts, likes_enabled, comment_counts, comments_enabled),
+                        columns=['video_id', 'view_count', 'like_count', 'likes_enabled',
+                                 'comment_count', 'comments_enabled'])
+
+    _export_video_data(member_name, data, export_data, 'video_stats')
+
+    return data
+
+
+def get_video_attributes(member_name, responses, export_data=True):
+    """Extracts video attributes for the specified Hololive Production member's videos from the API's responses.
+
+    This function extracts YouTube video attributes from YouTube Data API responses, exports a copy of the data
+    (unless instructed otherwise), and returns a Pandas DataFrame containing the collected data.
+
+    Args:
+        member_name: String specifying the name of the Hololive Production member whose videos data is being
+                     collected for.
+        responses: List of YouTube Data API responses to requests for video data.
+        export_data: Boolean specifying whether collected data is to be exported. Default = True.
+
+    Returns:
+        data: Pandas DataFrame containing video ids and corresponding video attributes for the current member.
+    """
+
+    video_ids = []
+    titles = []
+    publish_datetimes = []
+    durations = []
+    category_ids = []
+    is_live_broadcast = []
+    scheduled_start_times = []
+    scheduled_end_times = []
+    actual_start_times = []
+    actual_end_times = []
+
+    for response in responses:
+        results = response['items']
+        for result in results:
+            video_ids.append(result['id'])
+            titles.append(result['snippet']['title'])
+            publish_datetimes.append(result['snippet']['publishedAt'])
+            durations.append(result['contentDetails']['duration'])
+            category_ids.append(result['snippet']['categoryId'])
+
+            live_broadcast = 'liveStreamingDetails' in result
+            is_live_broadcast.append(live_broadcast)
+            if live_broadcast:
+                scheduled_start_times.append(result['liveStreamingDetails'].get('scheduledStartTime', np.NaN))
+                scheduled_end_times.append(result['liveStreamingDetails'].get('scheduledEndTime', np.NaN))
+                actual_start_times.append(result['liveStreamingDetails'].get('actualStartTime', np.NaN))
+                actual_end_times.append(result['liveStreamingDetails'].get('actualEndTime', np.NaN))
+            else:
+                scheduled_start_times.append(np.NaN)
+                scheduled_end_times.append(np.NaN)
+                actual_start_times.append(np.NaN)
+                actual_end_times.append(np.NaN)
+
+    data = pd.DataFrame(zip(video_ids, titles, publish_datetimes, durations,
+                            category_ids, is_live_broadcast,
+                            scheduled_start_times, scheduled_end_times, actual_start_times, actual_end_times),
+                        columns=['video_id', 'title', 'publish_datetime', 'duration',
+                                 'category_id', 'live_broadcast',
+                                 'scheduled_start_time', 'scheduled_end_time', 'actual_start_time', 'actual_end_time'])
+
+    _export_video_data(member_name, data, export_data, 'video_attributes')
+
+    return data
+
+
+def _export_video_data(member_name, data, export_data, filename):
+    """Exports collected YouTube video data.
+
+    This PRIVATE function exports collected YouTube video data to a text-based file located in the current session's
+    'Video' directory and with the specified file name.
+
+    Args:
+        member_name: String specifying the name of the Hololive Production member whose videos data is being
+                     collected for.
+        data: Pandas DataFrame containing YouTube video data.
+        export_data: Boolean specifying whether collected data is to be exported. Default = True.
+        filename: String specifying the name of the output file.
+
+    Returns:
+        None
+    """
+
+    if export_data is True and df.SESSION_PATH is not None:
+        member_name = member_name.replace(' ', '_')
+        export.export_dataframe(data, df.SESSION_PATH / 'Video' / member_name, f'{member_name.lower()}_{filename}')
---
+++ holoanalytics/utils/export.py
@@ -0,0 +1,116 @@
+"""Exporting of data
+
+This module provides general-purpose tools relating the export of data for this project.
+
+Functions:
+    - create_directory: Creates a directory in the specified parent directory using the specified name.
+    - create_session: Starts a new session.
+    - export_dataframe: Exports a Pandas DataFrame.
+"""
+
+import os
+from pathlib import Path
+from datetime import datetime
+from holoanalytics import definitions as df
+
+
+def create_directory(parent_dir_path, dir_name, add_date=True, output_message=True):
+    """Creates a directory in the specified parent directory using the specified name.
+
+    Args:
+        parent_dir_path: Path object specifying the absolute path to the directory that the new directory
+                         is to be created in.
+        dir_name: String specifying the name of the directory to be created.
+        add_date: Boolean specifying if the date is to be added to the directory name. Default = True.
+        output_message: Boolean specifying if an output message is to be printed. Default = True.
+
+    Returns:
+        full_path: Path object specifying the absolute path to the created directory.
+    """
+
+    # If 'add_data' is overwritten with any other value, assume date is not to be included.
+    # Add date only if default value is used.
+    if add_date is True:
+        current_date = f'{str(datetime.utcnow())[0:10]}'    # Format: YYYY-MM-DD
+        dir_name = f'{current_date}_{dir_name}'
+
+    full_path = parent_dir_path / dir_name
+
+    if full_path.exists():
+        full_path = _adjust_file_path(full_path)
+
+    full_path.mkdir(parents=True)
+
+    if output_message is True:
+        dir_name = full_path.name
+        parent_dir_name = parent_dir_path.name
+        print(f"'{dir_name}' has been successfully created in '{parent_dir_name}'.")
+
+    return full_path
+
+
+def _adjust_file_path(old_path):
+    """Adjusts the current absolute path, if it is taken, by attaching a number at the end, e.g. 'Path-2'
+
+    PRIVATE function.
+
+    Args:
+        old_path: Path object specifying the original absolute path.
+
+    Returns:
+        new_path: Path object specifying the new absolute path
+    """
+
+    counter = 2
+
+    while True:
+        new_path = Path(f'{old_path}-{counter}')
+        if not new_path.exists():
+            break
+        counter += 1
+
+    return new_path
+
+
+def create_session():
+    """Starts a new session and creates a directory to store all new files that will be created during said session.
+
+    Currently, creates session for new data collection using the YouTube Data API. Future update will support sessions
+    for other activities, including data collection using the Twitter and Twitch APIs.
+
+    Returns:
+        session_dir_path: Path object specifying the absolute path to the new session's directory.
+    """
+
+    session_id = 'YT-DAPI'
+    sessions_path = df.YT_DAPI_SESSIONS_PATH
+
+    session_dir_path = create_directory(sessions_path, f'{session_id}_Session', add_date=True)
+
+    return session_dir_path
+
+
+def export_dataframe(dataframe, dir_path, file_name, filetype='csv'):
+    """Exports a Pandas DataFrame to a text file based on the specified file type, file name, and location.
+
+    It is recommended to export the data to csv files.
+
+    Args:
+        dataframe: Pandas DataFrame to be exported.
+        dir_path: Path object specifying the absolute path to the directory that the output file is to be created in.
+        file_name: String specifying the name of the output file.
+        filetype: String specifying the type of the output file. Default = 'csv'.
+
+    Returns:
+        None
+    """
+
+    current_datetime = str(datetime.utcnow())
+    current_date = current_datetime[0:10]
+    current_time = current_datetime[11:16].replace(':', '')
+    full_filename = f'{current_date}-{current_time}_{file_name}.{filetype}'
+    full_path = os.path.join(dir_path, full_filename)
+    dataframe.to_csv(full_path, index=False)
+
+    print(f"'{full_filename}' has been successfully exported.")
+
