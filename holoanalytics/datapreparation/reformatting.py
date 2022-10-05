e8b4105bb1b9dc85347bdf264f4bbb9ef7b0947c
Kairis <80596049+Johnson-Tung@users.noreply.github.com>
2022-10-05 01:34:12 -0700

    feat(new): add reformatting functions
    Add functions to reformat collected data as part of data preparation.

--- holoanalytics/datapreparation/reformatting.py
+++ holoanalytics/datapreparation/reformatting.py
@@ -2,17 +2,7 @@ import datetime
 import pandas as pd
 from pandas.core.dtypes.common import is_timedelta64_dtype
 
-TIMES = ['scheduled_start_time', 'scheduled_end_time', 'actual_start_time', 'actual_end_time']
-
-
-def convert_duration_to_hms(duration):
-    timedelta = pd.Timedelta(duration)
-    days = timedelta.components.days
-    hours = timedelta.components.hours + days*24
-    minutes = timedelta.components.minutes
-    seconds = timedelta.components.seconds
-    hms = f'{hours}:{minutes}:{seconds}'
-    return hms
+START_END_TIMES = ['scheduled_start_time', 'scheduled_end_time', 'actual_start_time', 'actual_end_time']
 
 
 def zulutime_to_utc(date_time, show_tz=False):
@@ -23,7 +13,7 @@ def zulutime_to_utc(date_time, show_tz=False):
         show_tz: Boolean specifying whether the converted datetime should specify the timezone. Default = False.
 
     Returns:
-        new_date_time: String representing the datetime converted to UTC.
+        new_date_time: Datetime object representing the input time converted to UTC.
     """
 
     try:
@@ -38,6 +28,14 @@ def zulutime_to_utc(date_time, show_tz=False):
 
 
 def timedelta_to_hms(timedelta):
+    """Convert Pandas Timedelta to a string using H:M:S format.
+
+    Args:
+        timedelta: Pandas Timedelta object.
+
+    Returns:
+        hms: String specifying the timedelta in H:M:S format.
+    """
 
     days = timedelta.components.days
     hours = timedelta.components.hours + (days * 24)
@@ -50,14 +48,33 @@ def timedelta_to_hms(timedelta):
 
 
 def convert_times(video_attributes):
+    """Convert duration, start times, and end times in a YouTube Video Attributes DataFrame into Pandas Timedelta objects.
+
+    Args:
+        video_attributes: Pandas DataFrame containing YouTube video attribute data.
+
+    Returns:
+        video_attributes: Updated DataFrame where duration, start times, and end times have been converted to Pandas Timedelta objects.
+    """
 
     video_attributes = to_timedelta(video_attributes, 'duration')
-    video_attributes[TIMES] = video_attributes[TIMES].apply(pd.to_timedelta)
+
+    for time in START_END_TIMES:
+        video_attributes[time] = video_attributes[time].apply(zulutime_to_utc)
 
     return video_attributes
 
 
 def to_timedelta(dataframe, column_name):
+    """Convert a column containing duration data in the form of strings into Pandas Timedelta objects, if not already done.
+
+    Args:
+        dataframe: Pandas DataFrame.
+        column_name: String specifying the column name containing duration data.
+
+    Returns:
+        dataframe: Updated DataFrame where duration data is in the form of Pandas Timedelta objects.
+    """
 
     if not is_timedelta64_dtype(dataframe[column_name]):
         dataframe[column_name] = dataframe[column_name].apply(pd.to_timedelta)
