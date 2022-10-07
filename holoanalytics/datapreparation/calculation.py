e711afd3cbd787f28264d0e6aa6133d0d78de41a
Kairis <80596049+Johnson-Tung@users.noreply.github.com>
2022-10-07 15:57:18 -0700

    docs: add docstrings for calculation.py

--- holoanalytics/datapreparation/calculation.py
+++ holoanalytics/datapreparation/calculation.py
@@ -17,6 +17,24 @@ def live_broadcast_duration(dataframe):
 
 
 def extract_time_values(timedelta, use_days=False):
+    """Extract time values (days, hours, minutes, seconds) from a Pandas Timedelta object.
+
+    Results can be given with or without days, e.g. 1 day, 12 hours versus 36 hours.
+
+    Example: 2 days 03:04:05 = 2 days, 3 hours, 4 minutes, and 5 seconds or 51 hours, 4 minutes, and 5 seconds.
+
+    Args:
+        timedelta: Pandas Timedelta object.
+        use_days: Boolean specifying if days will be used. If True, days and hours will be given, e.g. 1 day , 12 hours.
+                  If False, hours will be given, e.g. 36 hours. Default = False.
+
+    Returns:
+        days: Integer specifying the number of days (Optional).
+        hours: Integer specifying the number of hours (or total number of hours if use_days is False)
+        minutes: Integer specifying the number of minutes.
+        seconds: Integer specifying the number of seconds.
+    """
+
     days = timedelta.components.days
     minutes = timedelta.components.minutes
     seconds = timedelta.components.seconds
@@ -29,7 +47,15 @@ def extract_time_values(timedelta, use_days=False):
         return hours, minutes, seconds
 
 
-def weekdays(video_attributes):  # Consider ignoring times except for actual start time
+def weekdays(video_attributes):
+    """Get the day of the week for YouTube videos' datetime data, e.g. 'actual_start_time'.
+
+    Args:
+        video_attributes: Pandas DataFrame containing YouTube video attribute data.
+
+    Returns:
+        video_attributes: Data updated with weekdays for video datetimes.
+    """
 
     for time in reform.START_END_TIMES:
         video_attributes[f'{time}_(weekday)'] = video_attributes[time].dt.day_name()
