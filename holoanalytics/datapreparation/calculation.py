f5295fd70a56e02e7f80f9185bb777d38d0db3b0
Kairis <80596049+Johnson-Tung@users.noreply.github.com>
2022-10-07 14:25:01 -0700

    feat(upgrade): improve calculation.py
    Simplify function names and importing. Add function to get weekdays.

--- holoanalytics/datapreparation/calculation.py
+++ holoanalytics/datapreparation/calculation.py
@@ -1,4 +1,7 @@
-def calc_live_broadcast_duration(dataframe):
+from holoanalytics.datapreparation import reformatting as reform
+
+
+def live_broadcast_duration(dataframe):
     """Calculate duration of live broadcasts for a YouTube Video Attribute DataFrame.
 
     Args:
@@ -10,7 +13,6 @@ def calc_live_broadcast_duration(dataframe):
 
     if 'live_broadcast_duration' not in dataframe:
         dataframe['live_broadcast_duration'] = dataframe['actual_end_time'] - dataframe['actual_start_time']
-
     return dataframe
 
 
@@ -25,3 +27,11 @@ def extract_time_values(timedelta, use_days=False):
     else:
         hours = timedelta.components.hours + (days * 24)
         return hours, minutes, seconds
+
+
+def weekdays(video_attributes):  # Consider ignoring times except for actual start time
+
+    for time in reform.START_END_TIMES:
+        video_attributes[f'{time}_(weekday)'] = video_attributes[time].dt.day_name()
+
+    return video_attributes
