6dad44c25e1b63055a97e78662c28b485e5506f3
Kairis <80596049+Johnson-Tung@users.noreply.github.com>
2022-11-28 13:06:39 -0800

    fix: prepare_youtube_data.py
    Add missing 'keyword_bank' variable

--- scripts/prepare_youtube_data.py
+++ scripts/prepare_youtube_data.py
@@ -1,5 +1,7 @@
 from holoanalytics.utils import importing
 from holoanalytics.datapreparation.extraction.video_keywords import extract_title_keywords
+from holoanalytics.datapreparation.extraction.video_keywords import eng_keyword_bank, jp_keyword_bank
+from holoanalytics.datapreparation import reformatting as ref
 from holoanalytics.datapreparation.classification.video_types import classify_video_type
 from holoanalytics.datapreparation.classification.content_types import classify_content_type
 
@@ -10,10 +12,11 @@ def main():
     importing.open_session(session_name)
 
     data = importing.import_video_data()
+    keyword_bank = ref.combine_keyword_banks(eng_keyword_bank, jp_keyword_bank)
 
     data = extract_title_keywords(data)
     data = classify_video_type(data)
-    data = classify_content_type(data)
+    data = classify_content_type(data, keyword_bank)
 
     return data
 
