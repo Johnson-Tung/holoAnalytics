27c44093d8f6a42bc55581eb0f0c8cf225dc4d1b
Kairis <80596049+Johnson-Tung@users.noreply.github.com>
2022-11-28 20:03:25 -0800

    feat(upgrade): update definitions.py
    Add path for folder containing keyword banks

--- holoanalytics/definitions.py
+++ holoanalytics/definitions.py
@@ -9,7 +9,9 @@ from pathlib import Path
 SOURCE_ROOT = Path(__file__).parent
 PROJECT_ROOT = SOURCE_ROOT.parent
 STARTING_DATA_FILE = SOURCE_ROOT / 'data' / 'starting' / 'holopro_members.csv'
+KEYWORD_BANKS_PATH = SOURCE_ROOT / 'data' / 'keyword_banks'
 RESULTS_PATH = PROJECT_ROOT / 'results'
 YT_DAPI_SESSIONS_PATH = RESULTS_PATH / 'collected' / 'YouTube' / 'YouTube-Data-API-Sessions'
+
 
 SESSION_PATH = None
