75efb578457f4e29c00cc5a62db4b7e744e3b5c0
Kairis <80596049+Johnson-Tung@users.noreply.github.com>
2023-02-27 23:58:35 -0800

    refactor: rename elements in prepare_youtube_data.py
    Rename import alias(es) and variable(s) to be more clear and
    informative.

--- scripts/prepare_youtube_data.py
+++ scripts/prepare_youtube_data.py
@@ -1,6 +1,6 @@
 from holoanalytics.utils import importing
 from holoanalytics.datapreparation.extraction.video_keywords import extract_title_keywords
-from holoanalytics.datapreparation import reformatting as ref
+from holoanalytics.datapreparation import reformatting as reform
 from holoanalytics.datapreparation.classification.video_types import classify_video_type
 from holoanalytics.datapreparation.classification.content_types import classify_content_type
 from holoanalytics.datapreparation import summary
@@ -11,18 +11,18 @@ def main():
     session_name = importing.request_session()
     importing.open_session(session_name)
 
-    data = importing.import_video_data()
+    video_data = importing.import_video_data()
     keyword_banks = importing.import_keyword_banks('english', 'japanese', 'indonesian')
 
-    data = ref.reformat_video_data(data)
+    video_data = reform.reformat_video_data(video_data)
 
-    data = extract_title_keywords(data, keyword_banks)
-    data = classify_video_type(data)
-    data = classify_content_type(data, keyword_banks)
+    video_data = extract_title_keywords(video_data, keyword_banks)
+    video_data = classify_video_type(video_data)
+    video_data = classify_content_type(video_data, keyword_banks)
 
-    channel_data = summary.summarize_video_data(data)
+    channel_data = summary.summarize_video_data(video_data)
 
-    return channel_data, data
+    return channel_data, video_data
 
 
 if __name__ == '__main__':
