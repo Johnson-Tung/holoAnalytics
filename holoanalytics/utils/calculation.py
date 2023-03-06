6f64e92282b811fbb3a5d3fb33fe1080c0d3f1bd
Kairis <80596049+Johnson-Tung@users.noreply.github.com>
2023-03-06 00:40:01 -0800

    feat(new): add calculate_interval()
    Add a function to calculate an appropriate interval to use for a plot
    by using the largest value, e.g. 450,000 will return an interval of
    100,000.

--- holoanalytics/utils/calculation.py
+++ holoanalytics/utils/calculation.py
@@ -4,3 +4,11 @@ import math
 def round_up_nearest_x(number, x):
 
     return math.ceil(number / x) * x
+
+
+def calculate_interval(number, factor=1):
+
+    digits = int(math.log10(number)) + 1
+    interval = int(int('1' + '0' * (digits - 1)) / factor)
+
+    return interval
