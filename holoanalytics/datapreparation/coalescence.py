37162f8a0b2f5c75c1fb447a6c85a1f3ce695c3b
Kairis <80596049+Johnson-Tung@users.noreply.github.com>
2023-06-12 09:47:31 -0700

    feat(wip): add ability to coalesce video data
    Add the ability to combine video attribute, video stat, and video type
    data for all members into a single dataset.

---
+++ holoanalytics/datapreparation/coalescence.py
@@ -0,0 +1,32 @@
+from functools import reduce
+
+import pandas as pd
+
+
+def coalesce_video_data(starting_member_data, member_video_data):
+    overall_video_data = {}
+    merged_video_datasets = []
+
+    member_names = starting_member_data['name']
+
+    for member_name in member_names:
+        member_name_key = member_name.replace(' ', '_')
+        video_data = member_video_data[member_name_key]
+
+        video_attributes = video_data['video_attributes']['data']
+        video_stats = video_data['video_stats']['data']
+        video_types = video_data['video_types']['data']
+
+        merged_video_data = reduce(lambda x, y: pd.merge(x, y, on='video_id'), [video_attributes, video_stats,
+                                                                                video_types])
+
+        merged_video_data.insert(0, 'member_name', member_name)
+
+        merged_video_datasets.append(merged_video_data)
+
+    overall_video_data['data'] = pd.concat(merged_video_datasets, ignore_index=True)
+
+    member_video_data['Overall'] = overall_video_data
+
+    return member_video_data
+
